3be778856ef227a9bc84331fba6be21224071098
Evan Stein <evan@karmamethod.com>
2020-12-28 16:20:12 -0500

    updating index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,14 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Who is Evan Stein?" />
+    <h1>Greetings Humans!</h1>
+    <p>If you're reading this, you are probably wondering who Evan Stein is.</p>
+    <p>Or maybe you are wondering who I am.</p>
+    <p>Well, so am I.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
